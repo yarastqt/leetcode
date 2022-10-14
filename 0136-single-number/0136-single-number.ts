f867c503b2fe490739e4037f5c4e1243001070d2
Eugene Tropin <yarastqt@gmail.com>
2022-10-15 01:06:17 +0400

    Time: 66 ms (98.73%), Space: 48.4 MB (29.61%) - LeetHub

---
+++ 0136-single-number/0136-single-number.ts
@@ -0,0 +1,13 @@
+function singleNumber(nums: number[]): number {
+  const set = new Set<number>();
+
+  for (const num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+
+  return Array.from(set.values())[0];
+}
